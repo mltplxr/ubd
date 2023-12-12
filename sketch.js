799b4a1ad2a0d570ddff8097e96c8c99c283fafb
mltplxr <128910989+mltplxr@users.noreply.github.com>
2023-12-12 09:09:59 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+let id = Math.floor(Math.random() * 1000000);
+let svg;
+
+function preload() {
+  svg = loadImage("helios.svg");
+}
+function setup() {
+  createCanvas(400, 600);
+
+  image(svg, 10, 0, 360, 600);
+
+  strokeWeight(0.1);
+  noFill(0);
+  
+   
+  
+}
+
+  
+function draw() {
+
+
+for (let i = 0; i < height; i += 1) {// manipulate topography
+
+beginShape();
+
+for (let j = 0; j < width; j += 1.8) {// manipulate topography
+let noiseVal = noise(j/200, i/40); // 100, 100 > for smoothness 
+
+vertex(j, i - noiseVal * 200); // 200 < for smoothness
+
+}
+    endShape();
+  }
+
+
+}
